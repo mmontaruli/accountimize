6943cb9542e137f7a5de17b6798c1b55a8797118
Matt Montaruli <matthew.montaruli@gmail.com>
2011-10-19 21:00:28 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Accountimize::Application.config.secret_token = 'fcb694ef349fb6b3b5692c2e6176a615c60679c2fecd534f21a064ed5776766ff3136132b77c9b7093e2fd4803bc639a1151b51a10882714ccaa026cc56a6586'
