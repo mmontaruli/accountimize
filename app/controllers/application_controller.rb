40b2a00937af29a982eb452bf27d9ec6b43804ae
Matt Montaruli <matthew.montaruli@gmail.com>
2011-10-22 18:06:18 -0400

    Minor adjustment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,9 @@ class ApplicationController < ActionController::Base
   before_filter :get_section_name
   protect_from_forgery
   
-private
-  def get_section_name
-    @section_name = params[:controller]
-  end
+  private
+    
+    def get_section_name
+      @section_name = params[:controller]
+    end
 end
