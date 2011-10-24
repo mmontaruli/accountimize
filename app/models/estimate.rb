4f99469980e89a82f615734d97b12386e7d5f4ed
Matt Montaruli <matthew.montaruli@gmail.com>
2011-10-23 20:50:53 -0400

    Added line item totals and conditional estimate total

--- app/models/estimate.rb
+++ app/models/estimate.rb
@@ -1,7 +1,11 @@
 class Estimate < ActiveRecord::Base
-  has_many :line_items
+  has_many :line_items, :dependent => :destroy
   
   validates :date, :number, :presence => true
   validates :number, :numericality => {:greater_than_or_equal_to => 1}
+  
+  def total_price
+    line_items.where(:is_enabled => true).to_a.sum { |item| item.total_price }
+  end
   
 end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,10 @@
 class LineItem < ActiveRecord::Base
   belongs_to :estimate
   
-  validates :name, :presence => true
+  validates :name, :estimate_id, :presence => true
+  validates :estimate_id, :numericality => {:greater_than_or_equal_to => 1}
+  
+  def total_price
+    unit_price * quantity
+  end
 end
