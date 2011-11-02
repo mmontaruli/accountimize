819daf05a358cdb52c54d43e1a3713f239953435
Matt Montaruli <matthew.montaruli@gmail.com>
2011-11-02 00:43:56 -0400

    Fixed estimates editing form with nested line_items model

--- app/models/estimate.rb
+++ app/models/estimate.rb
@@ -1,6 +1,8 @@
 class Estimate < ActiveRecord::Base
   has_many :line_items, :dependent => :destroy
   
+  accepts_nested_attributes_for :line_items, :allow_destroy => true
+  
   validates :date, :number, :presence => true
   validates :number, :numericality => {:greater_than_or_equal_to => 1}
   
