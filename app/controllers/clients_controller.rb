e258c99e15821ac4736519dbf944304b176ab822
Matt Montaruli <matthew.montaruli@gmail.com>
2011-11-23 15:33:18 -0500

    Added client model validations

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -44,7 +44,7 @@ class ClientsController < ApplicationController
 
     respond_to do |format|
       if @client.save
-        format.html { redirect_to @client, notice: 'Client was successfully created.' }
+        format.html { redirect_to @client, :flash => {notice: 'Client was successfully created.', :status => 'success'} }
         format.json { render json: @client, status: :created, location: @client }
       else
         format.html { render action: "new" }
@@ -60,7 +60,7 @@ class ClientsController < ApplicationController
 
     respond_to do |format|
       if @client.update_attributes(params[:client])
-        format.html { redirect_to @client, notice: 'Client was successfully updated.' }
+        format.html { redirect_to @client, :flash => {notice: 'Client was successfully updated.', :status => 'success'} }
         format.json { head :ok }
       else
         format.html { render action: "edit" }
