e0e68538f4ebb5c014f8d6725ff5b11312cddfa0
Matt Montaruli <matthew.montaruli@gmail.com>
2012-07-03 11:16:09 -0400

    Completed code to fix issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   skip_before_filter :authorize
+  before_filter :restrict_client_access
   def new
     @user = User.new
     @client = Client.find_by_id(params[:client_id])
@@ -13,6 +14,17 @@ class UsersController < ApplicationController
       redirect_to client_path(@client), :flash => {:notice => "Signed up!", :status => "success"}
     else
       render "new"
+    end
+  end
+  private
+  def restrict_client_access
+    if params[:client_id]
+      @target_client = Client.find_by_id(:client_id)
+      unless signed_in_client.is_account_master
+        if @target_client != signed_in_client
+          redirect_to site_url
+        end
+      end
     end
   end
 end
