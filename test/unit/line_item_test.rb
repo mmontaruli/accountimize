bb96be534624ca104695461df3c1afd330108e10
Matt Montaruli <matthew.montaruli@gmail.com>
2012-08-25 18:26:59 -0400

    Removed test directory--rspec migration complete

--- test/unit/line_item_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class LineItemTest < ActiveSupport::TestCase
-  #test "line_item attributes must not be empty" do
-  #  line_item = LineItem.new
-  #  assert line_item.invalid?
-  #  assert line_item.errors[:name].any?
-  #end
-end
