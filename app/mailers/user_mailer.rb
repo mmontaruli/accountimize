3ad398cdb31c57f69955a814182352f4a2b0301e
Matt Montaruli <matthew.montaruli@gmail.com>
2012-09-03 20:39:03 -0400

    Changed password reset email sender

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,17 +1,8 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "no-reply@accountimize.com"
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.password_reset.subject
-  #
   def password_reset(user)
     @user = user
     mail to: user.email, subject: "Password reset"
-
-    #@greeting = "Hi"
-
-    #mail to: "to@example.org"
   end
 end
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -7,20 +7,16 @@ describe UserMailer do
     @user.save!
   end
   describe "password_reset" do
-    #user = create(:user)
-    #let(:user) { mock_model(User, :email => 'lucas@email.com') }
     let(:mail) { UserMailer.password_reset(@user) }
 
     it "renders the headers" do
 
       mail.subject.should eq("Password reset")
-      #mail.to.should eq(["to@example.org"])
       mail.to.should == [@user.email]
-      #mail.from.should eq(["from@example.com"])
+      mail.from.should == ["no-reply@accountimize.com"]
     end
 
     it "renders the body" do
-      #mail.body.encoded.should match("Hi")
       mail.body.encoded.should match("To reset your password, click the URL below.")
     end
   end
