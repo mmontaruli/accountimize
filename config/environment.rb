6224f29c607a458ec192f2c6c50705c6fc3b4c07
Matt Montaruli <matthew.montaruli@gmail.com>
2013-02-25 21:09:11 -0500

    Small mail configure fix

--- config/environment.rb
+++ config/environment.rb
@@ -5,14 +5,14 @@ require File.expand_path('../application', __FILE__)
 Accountimize::Application.initialize!
 
 # Configure production servers to send email
-if Rails.env.production?
-	config.action_mailer.delivery_method = :smtp
-	ActionMailer::Base.smtp_settings = {
-  		:address  => "smtp.mandrillapp.com",
-  		:port  => 587,
-  		:user_name  => "app11566888@heroku.com",
-  		:password  => "Gp4sScBcJHnYUaiXi3wLKw",
-  		:authentication  => :login
-	}
-	config.action_mailer.raise_delivery_errors = true
-end
+# if Rails.env.production?
+# 	config.action_mailer.delivery_method = :smtp
+# 	ActionMailer::Base.smtp_settings = {
+#   		:address  => "smtp.mandrillapp.com",
+#   		:port  => 587,
+#   		:user_name  => "app11566888@heroku.com",
+#   		:password  => "Gp4sScBcJHnYUaiXi3wLKw",
+#   		:authentication  => :login
+# 	}
+# 	config.action_mailer.raise_delivery_errors = true
+# end
