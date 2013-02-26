ae367d5a10b04f370442fca7b1df0efe76cf46f8
Matt Montaruli <matthew.montaruli@gmail.com>
2013-02-25 21:19:52 -0500

    Removed incorrect mail config code

--- config/environment.rb
+++ config/environment.rb
@@ -3,16 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Accountimize::Application.initialize!
-
-# Configure production servers to send email
-# if Rails.env.production?
-# 	config.action_mailer.delivery_method = :smtp
-# 	ActionMailer::Base.smtp_settings = {
-#   		:address  => "smtp.mandrillapp.com",
-#   		:port  => 587,
-#   		:user_name  => "app11566888@heroku.com",
-#   		:password  => "Gp4sScBcJHnYUaiXi3wLKw",
-#   		:authentication  => :login
-# 	}
-# 	config.action_mailer.raise_delivery_errors = true
-# end
